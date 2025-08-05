270e5d2428f15c148beb9b59cda05cd7154cb7da
[dyad] <git@dyad.sh>
2025-08-05 13:44:32 +0200

    [dyad] Adding English language support using i18next - wrote 7 file(s), added i18next, react-i18next, i18next-browser-languagedetector, i18next-http-backend package(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,22 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { useLanguageDetection } from "@/hooks/useLanguageDetection";
+import React, { Suspense } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Index from "./pages/Index";
 import StatusPage from "./pages/Status";
 import NotFound from "./pages/NotFound";
-import { ThemeProvider } from "@/components/theme-provider";
+import './i18n';
 
-const queryClient = new QueryClient();
-
-// Composant wrapper pour la détection de langue
-const AppWrapper = () => {
-  useLanguageDetection();
-  
+function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <ThemeProvider defaultTheme="dark" storageKey="ui-theme">
-        <TooltipProvider>
-          <Toaster />
-          <Sonner />
-          <BrowserRouter>
-            <Routes>
-              <Route path="/" element={<Index />} />
-              <Route path="/status" element={<StatusPage />} />
-              {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </BrowserRouter>
-        </TooltipProvider>
-      </ThemeProvider>
-    </QueryClientProvider>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/status" element={<StatusPage />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </Suspense>
   );
-};
-
-const App = () => (
-  <AppWrapper />
-);
+}
 
 export default App;
