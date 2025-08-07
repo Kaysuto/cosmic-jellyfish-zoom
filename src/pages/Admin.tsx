fea4b6585f2ad6f34bd34b7d0a93465177514156
[dyad] <contact@kaysuto.fr>
2025-08-07 16:33:25 +0200

    [dyad] Amélioration de l'animation du panneau d'administration - wrote 1 file(s)

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -117,10 +117,10 @@ const Admin = () => {
         <AnimatePresence mode="wait">
           <motion.div
             key={location.pathname}
-            initial={{ opacity: 0, y: 20 }}
-            animate={{ opacity: 1, y: 0 }}
-            exit={{ opacity: 0, y: -20 }}
-            transition={{ duration: 0.2 }}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.3, ease: 'easeInOut' }}
           >
             <Outlet />
           </motion.div>
