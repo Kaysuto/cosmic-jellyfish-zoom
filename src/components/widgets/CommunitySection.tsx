9d9673a8da9ace8b19d16e9b64587e80af18e9a8
[dyad] <contact@kaysuto.fr>
2025-08-21 18:49:04 +0200

    [dyad] Réintégration du widget musical - wrote 3 file(s)

--- src/components/widgets/CommunitySection.tsx
+++ src/components/widgets/CommunitySection.tsx
@@ -1,12 +1,12 @@
 import DiscordWidget from './DiscordWidget';
+import SpotifyWidget from './SpotifyWidget';
 
 const CommunitySection = () => {
   return (
     <div className="container mx-auto px-4 py-8 border-t border-white/10">
-      <div className="flex justify-center">
-        <div className="w-full md:w-1/2">
-          <DiscordWidget />
-        </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-8 items-start">
+        <SpotifyWidget />
+        <DiscordWidget />
       </div>
     </div>
   );
